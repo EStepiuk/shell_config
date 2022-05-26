b9d7a7f056a822786c952770d047142ebb92737e
Yevhen Stepiuk <stepyuk.android@gmail.com>
2022-05-26 23:30:33 +0300

    Add aliases

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,9 @@
 alias re-zshrc='source ~/.zshrc'
+
+flutter_build_runner() {
+  flutter pub run build_runner $1 --delete-conflicting-outputs
+}
+
+alias flutter_build_runner_build='flutter_build_runner build'
+alias flutter_build_runner_watch='flutter_build_runner watch'
+
