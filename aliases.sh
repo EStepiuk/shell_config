1dbc78d43274414e0c14d47669b30e66976c17e8
Yevhen Stepiuk <stepyuk.android@gmail.com>
2022-10-09 14:23:36 +0300

    Refactor aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,24 +1,31 @@
+# Base
 alias re-zshrc='source ~/.zshrc'
 
-flutter-build-runner() {
-  flutter pub run build_runner "$1" --delete-conflicting-outputs
-}
-
-alias flutter-build-runner-build='flutter-build-runner build'
-alias flutter-build-runner-watch='flutter-build-runner watch'
-
+# Default tools replacement
 alias cat='bat'
+alias vim='nvim'
 
+# Env
+alias brew-dump='brew bundle dump --force --file="$SHELL_CONFIG/Brewfile"'
+
+# Git
 alias batdiff='git diff --name-only --relative --diff-filter=d | xargs bat --diff'
 alias submodules='git submodule update --init --recursive'
 
-adb-wifi() {
-  adb tcpip 5555
-  adb connect $1
-}
-
+# Misc
 # Fixes HDD issue with fsck.
 # You need to already fix this HDD 😅
 alias fix-hdd='sudo pkill -9 fsck'
 
-alias brew-dump='brew bundle dump --force --file="$SHELL_CONFIG/Brewfile"'
+# Flutter
+flutter-build-runner() {
+  flutter pub run build_runner "$1" --delete-conflicting-outputs
+}
+alias flutter-build-runner-build='flutter-build-runner build'
+alias flutter-build-runner-watch='flutter-build-runner watch'
+
+# Android
+adb-wifi() {
+  adb tcpip 5555
+  adb connect "$1"
+}
